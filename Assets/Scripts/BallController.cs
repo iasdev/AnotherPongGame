e994038a7c5656a1a88b294cddca6fd378a00a45
iasdev <iarjona1995@gmail.com>
2021-04-04 21:53:54 +0200

    version 1.2.0

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,41 +10,106 @@ public class BallController : MonoBehaviour
     private float originalSpeed;
     private Vector3 zMovement = Vector3.forward;
     private Vector3 xMovement = Vector3.right;
+    private bool anyPlayerWin = false;
 
     public float waitTimeForNextGame = 3;
     private float remainingWaitTime = 0;
 
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("TopSide")) {
+    public AudioClip ballCollision;
+    public AudioClip goal;
+    public AudioClip endGame;
+    private AudioSource audioSource;
+
+    public string maxScoreToWin;
+    public TextMesh leftScore;
+    public TextMesh rightScore;
+    public TextMesh messages;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TopSide"))
+        {
             zMovement = Vector3.back;
-        } else if (other.CompareTag("DownSide")) {
+        }
+        else if (other.CompareTag("DownSide"))
+        {
             zMovement = Vector3.forward;
-        } else if (other.CompareTag("RightPad")) {
+        }
+        else if (other.CompareTag("RightPad"))
+        {
             xMovement = Vector3.left;
-            speed += speedIncrement;
-        } else if (other.CompareTag("LeftPad")) {
+            OnAnyPadTouch();
+        }
+        else if (other.CompareTag("LeftPad"))
+        {
             xMovement = Vector3.right;
-            speed += speedIncrement;
-        } else if (other.CompareTag("GoalBand")) {
+            OnAnyPadTouch();
+        }
+        else if (other.CompareTag("GoalBand"))
+        {
             transform.position = Vector3.zero;
             zMovement = Random.value > 0.5 ? Vector3.back : Vector3.forward;
-            xMovement = Random.value > 0.5 ? Vector3.right : Vector3.left; 
+            xMovement = Random.value > 0.5 ? Vector3.right : Vector3.left;
             speed = originalSpeed;
-            
             remainingWaitTime = waitTimeForNextGame;
+
+            TextMesh score = other.gameObject.name == "LeftGoalBand" ? leftScore : rightScore;
+            score.text = (int.Parse(score.text) + 1).ToString();
+
+            if (leftScore.text == maxScoreToWin)
+            {
+                OnPlayerWin("Verde");
+            }
+            else if (rightScore.text == maxScoreToWin)
+            {
+                OnPlayerWin("Azul");
+            }
+            else
+            {
+                audioSource.clip = goal;
+                audioSource.Play();
+            }
         }
     }
 
-    void Start() {
+    private void OnAnyPadTouch()
+    {
+        speed += speedIncrement;
+        audioSource.clip = ballCollision;
+        audioSource.Play();
+    }
+
+    private void OnPlayerWin(string player)
+    {
+        anyPlayerWin = true;
+        audioSource.clip = endGame;
+        audioSource.Play();
+        messages.text = "Ha ganado el jugador " + player;
+    }
+
+    void Start()
+    {
         originalSpeed = speed;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (remainingWaitTime > 0) {
+        if (remainingWaitTime > 0)
+        {
             remainingWaitTime -= Time.deltaTime;
-        } else {
+        }
+        else
+        {
+            if (anyPlayerWin)
+            {
+                anyPlayerWin = false;
+                messages.text = "";
+                leftScore.text = "0";
+                rightScore.text = "0";
+            }
+
             transform.Translate(zMovement * speed * Time.deltaTime);
             transform.Translate(xMovement * speed * Time.deltaTime);
         }
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Another Pong Game made with Unity and &lt;3
 ## Version 1.1.0
 - Basic gameplay. (1 vs 1 same computer)
 - Initial Android support. (1 vs 1 same mobile)
+
+## Version 1.2.0
+- Basic sound usage (on player goal/win, on ball collision)
+- Score board
